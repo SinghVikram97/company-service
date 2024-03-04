8e8ffbf70c968888b33aa3a082af435e6e51e7af
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-03-04 16:11:36 -0500

    implemented retry mechanism

--- src/main/java/com/vikram/companyservice/client/JobServiceClient.java
+++ src/main/java/com/vikram/companyservice/client/JobServiceClient.java
@@ -2,6 +2,7 @@ package com.vikram.companyservice.client;
 
 import com.vikram.companyservice.model.Job;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import java.util.List;
 public interface JobServiceClient {
     @GetMapping("${job.service.basepath}/company")
     @CircuitBreaker(name = "jobBreaker", fallbackMethod = "getAllJobsByCompanyFallback")
+    @Retry(name = "jobServiceRetry")
     ResponseEntity<List<Job>> getAllJobsByCompany(@RequestParam("company_id") Long companyId);
 
     default ResponseEntity<List<Job>> getAllJobsByCompanyFallback(Long companyId, Exception exception) {
--- src/main/java/com/vikram/companyservice/client/ReviewServiceClient.java
+++ src/main/java/com/vikram/companyservice/client/ReviewServiceClient.java
@@ -1,7 +1,9 @@
 package com.vikram.companyservice.client;
 
 import com.vikram.companyservice.model.Review;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import java.util.List;
 public interface ReviewServiceClient {
     @GetMapping("${review.service.basepath}/company")
     @CircuitBreaker(name = "reviewBreaker", fallbackMethod = "getAllReviewsForCompanyFallback")
+    @Retry(name = "reviewServiceRetry")
     ResponseEntity<List<Review>> getAllReviewsForCompany(@RequestParam("company_id") Long companyId);
 
     default ResponseEntity<List<Review>> getAllReviewsForCompanyFallback(Long companyId, Exception exception) {
