78dc1587eadefe83b8218ef1fba1408eda7d880f
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-03-05 18:45:22 -0500

    implemented deadletter queue for review service

--- src/main/java/com/vikram/companyservice/messaging/RabbitMQConfiguration.java
+++ src/main/java/com/vikram/companyservice/messaging/RabbitMQConfiguration.java
@@ -1,10 +1,7 @@
 package com.vikram.companyservice.messaging;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.Declarables;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -22,10 +19,17 @@ public class RabbitMQConfiguration {
         // Fanout exchange i.e. send messages to all queues bound to this exchange irrespective of routing key
         return new Declarables(
                 new FanoutExchange("x.delete-company"),
+                new DirectExchange("x.delete-reviews-failure"),
                 new Queue("q.delete-jobs"),
-                new Queue("q.delete-reviews"),
+                QueueBuilder.durable("q.delete-reviews")
+                        .withArgument("x-dead-letter-exchange","x.delete-reviews-failure")
+                        .withArgument("x-dead-letter-routing-key","delete-reviews-fallback") // send to dead letter exchange with this routing key
+                        .build(),
+                new Queue("q.delete-reviews-dlq"),
                 new Binding("q.delete-jobs", Binding.DestinationType.QUEUE, "x.delete-company","delete-jobs",null),
-                new Binding("q.delete-reviews", Binding.DestinationType.QUEUE, "x.delete-company","delete-reviews",null)
+                new Binding("q.delete-reviews", Binding.DestinationType.QUEUE, "x.delete-company","delete-reviews",null),
+                // accept if this routing key comes from exchange
+                new Binding("q.delete-reviews-dlq", Binding.DestinationType.QUEUE, "x.delete-reviews-failure","delete-reviews-fallback",null)
         );
     }
 
