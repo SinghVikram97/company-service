39b7f80292726d60300f7712170c53236b921b53
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-03-05 19:09:45 -0500

    added dlq for job service

--- src/main/java/com/vikram/companyservice/messaging/RabbitMQConfiguration.java
+++ src/main/java/com/vikram/companyservice/messaging/RabbitMQConfiguration.java
@@ -17,19 +17,26 @@ public class RabbitMQConfiguration {
     @Bean
     public Declarables createCompanyExchangeAndQueues(){
         // Fanout exchange i.e. send messages to all queues bound to this exchange irrespective of routing key
+        // Direct exchange i.e. queue will take messages with given routing key from specified exchange
         return new Declarables(
                 new FanoutExchange("x.delete-company"),
                 new DirectExchange("x.delete-reviews-failure"),
-                new Queue("q.delete-jobs"),
+                new DirectExchange("x.delete-jobs-failure"),
+                QueueBuilder.durable("q.delete-jobs")
+                        .withArgument("x-dead-letter-exchange","x.delete-jobs-failure")
+                        .withArgument("x-dead-letter-routing-key","delete-jobs-fallback") // send to dead letter exchange with this routing key
+                        .build(),
                 QueueBuilder.durable("q.delete-reviews")
                         .withArgument("x-dead-letter-exchange","x.delete-reviews-failure")
                         .withArgument("x-dead-letter-routing-key","delete-reviews-fallback") // send to dead letter exchange with this routing key
                         .build(),
                 new Queue("q.delete-reviews-dlq"),
+                new Queue("q.delete-jobs-dlq"),
                 new Binding("q.delete-jobs", Binding.DestinationType.QUEUE, "x.delete-company","delete-jobs",null),
                 new Binding("q.delete-reviews", Binding.DestinationType.QUEUE, "x.delete-company","delete-reviews",null),
                 // accept if this routing key comes from exchange
-                new Binding("q.delete-reviews-dlq", Binding.DestinationType.QUEUE, "x.delete-reviews-failure","delete-reviews-fallback",null)
+                new Binding("q.delete-reviews-dlq", Binding.DestinationType.QUEUE, "x.delete-reviews-failure","delete-reviews-fallback",null),
+                new Binding("q.delete-jobs-dlq", Binding.DestinationType.QUEUE, "x.delete-jobs-failure","delete-jobs-fallback",null)
         );
     }
 
